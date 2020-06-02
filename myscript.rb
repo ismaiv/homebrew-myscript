855e2f35b98ec1d4796c25e17266ff71bcfeaf9f
ismaiv <valdet_is@hotmail.com>
2020-06-02 14:52:24 +0200

    Create myscript.rb

---
+++ myscript.rb
@@ -0,0 +1,12 @@
+class Myscript < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/ismaiv/myscript/archive/v1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "9f2ba2a754167978b9b78c235ecd74904d0ee4c51771975495d614c1a643b486"
+
+  def install
+      bin.install "myscript.rb"
+  end
+
+end

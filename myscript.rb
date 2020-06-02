7aab6e2bde55e3e923fd17463df43553a30fca60
ismaiv <valdet_is@hotmail.com>
2020-06-02 15:29:00 +0200

    Update myscript.rb

--- myscript.rb
+++ myscript.rb
@@ -1,8 +1,8 @@
 class Myscript < Formula
   desc ""
   homepage ""
-  url "https://github.com/ismaiv/myscript/archive/v2.0.0.tar.gz"
-  version "1.0.0"
+  url "https://github.com/ismaiv/myscript/archive/v3.0.0.tar.gz"
+  version "3.0.0"
   sha256 "72252cb5b5fefc15c62bd648ba1c01c23ac9279ecf20e15745a1342f180bbe02"
 
   def install

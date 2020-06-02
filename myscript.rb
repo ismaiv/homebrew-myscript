0e330a0c1f3ac2bc0a033c6c17cb6aa1705c967e
ismaiv <valdet_is@hotmail.com>
2020-06-02 15:22:03 +0200

    Update myscript.rb

--- myscript.rb
+++ myscript.rb
@@ -3,7 +3,7 @@ class Myscript < Formula
   homepage ""
   url "https://github.com/ismaiv/myscript/archive/v2.0.0.tar.gz"
   version "1.0.0"
-  sha256 "c90be871ee28e68efc302eb025471d2b8ba3f38c5114508471a8643f1f47dc10"
+  sha256 "72252cb5b5fefc15c62bd648ba1c01c23ac9279ecf20e15745a1342f180bbe02"
 
   def install
     bin.install "myscript"
